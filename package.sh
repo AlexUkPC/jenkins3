530072a3d6f90ddd31706c1e8d661bae9cacbd2e
AlexUkPC <alexrogna@yahoo.com>
2022-02-08 20:24:40 +0000

    build args

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 VERSION=${1:-latest}
-docker build -f Dockerfile.prod -t alexrogna/jenkins3_web:prod -t alexrogna/jenkins3_web:${VERSION} .
+docker build -f Dockerfile.prod -t alexrogna/jenkins3_web:prod -t alexrogna/jenkins3_web:${VERSION} --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) .
